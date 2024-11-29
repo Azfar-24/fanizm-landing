f2353b111509a18079fbb2860f17ce8748106689
Amit-34 <Amitramsoni111@gmail.com>
2024-11-30 03:32:45 +0530

    worked on hero section

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -13,25 +13,29 @@ const Header = () => {
         <div className="tagline">
           <h1>
             UNLOCK MASSIVE <span className="highlight">CASH REWARDS</span>
+            GET STARTED TODAY!!!
           </h1>
-          <p>GET STARTED TODAY!!!</p>
+       
         </div>
 
         <div className="phone-mockup">
-          <img src="/assets/mockup.png" alt="App Preview" />
-          <div className="quiz-box">
+          <img src="images/hero-img.png" alt="App Preview" />
+          {/* <div className="quiz-box">
             <p>“Pushpa flower nahi… Pushpa _____ hai!”</p>
             <div className="options">
               <button className="option">FIRE!</button>
               <button className="option">WILDFIRE!!!</button>
             </div>
-          </div>
+          </div> */}
         </div>
 
         <div className="cta-section">
+          <div className="cta-top-text">
+            <img src="images/coins.png" alt="coins"/>
           <p>
-            <span className="highlight">Get upto ₹50 bonus</span> on sign up!!!
+            Get upto <span className="highlight"><sup>₹</sup>50 bonus</span> on sign up!!!
           </p>
+          </div>
           <button className="download-btn">DOWNLOAD NOW</button>
         </div>
       </div>
